fa0cb2e34cfd45d85993b38bcab53ee9410280b3
Dafa Hentra <63453455+dafahentra@users.noreply.github.com>
2024-05-06 09:19:47 +0700

    Update page.jsx

--- app/(in-progress)/about/page.jsx
+++ app/(in-progress)/about/page.jsx
@@ -5,7 +5,7 @@ import { Transition } from '@/layout';
 export const metadata = {
   title: 'About',
   description:
-    'Helping brands thrive in the digital world. Located in The Netherlands. Delivering tailor-made digital designs and building interactive websites from scratch. © Code by Dennis',
+    'Helping brands thrive in the digital world. Located in The Netherlands. Delivering tailor-made digital designs and building interacti',
 };
 
 export default function About() {
